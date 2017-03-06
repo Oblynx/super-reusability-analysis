2dc46e93f50eed84080fcf08f58a0aca210ea5e4
Konstantinos Samaras-Tsakiris <ksamtsak@gmail.com>
2017-03-06 04:21:31 +0200

    Add SVM rules evaluation

--- code/+utils/showdistribution.m
+++ code/+utils/showdistribution.m
@@ -2,11 +2,13 @@ function showdistribution(data, reject_percentile)
 
 discreteness= length(data)/length(unique(data));
 data= data(data <= prctile(data,reject_percentile));
+%{
 if discreteness > 1000
   fprintf('[showdistribution]: bar graph (discreteness=%f)\n', discreteness);
   x= tabulate(data); bar(x(:,1),x(:,2));
 else
   fprintf('[showdistribution]: histogram\n');
+%}
   %histogram(data,'binmethod','integers');
-  histogram(data);
+  histogram(data,'normalization','pdf');
 end

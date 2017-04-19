4718ade822bd3378c03902b11afec54a9efdabb0
Konstantinos Samaras-Tsakiris <ksamtsak@gmail.com>
2017-04-19 13:16:26 +0300

    Remove redundant code from scorer

--- code/+model/Scorer.m
+++ code/+model/Scorer.m
@@ -7,10 +7,10 @@ properties (SetAccess= immutable)
 end
 properties
   params= struct( ...
-    'hiddenLayerSize',[20 5],...   % Size of each hidden layer
+    'hiddenLayerSize',[80 8],...   % Size of each hidden layer
     'trainFcn','trainlm',...        % Training function (trainlm,trainscg,trainbr)
     'performFcn','mse',...          % Error function
-    'max_fail',10,...               % Terminate if validation increases for this many epochs
+    'max_fail',12,...               % Terminate if validation increases for this many epochs
     'ratios', [0.7 0.15 0.15] ...
     );
 end
@@ -20,7 +20,7 @@ methods
     this@model.Model('code/+model/Scorer_storedmodel.mat');
   end
 %
-  function evalResults= train(this, dataset, targetset, repoStarts)
+  function evalResults= train(this, dataset, targetset)
   % After training, the model is stored in the 'storedmodelpath' file and can be
   % used after MATLAB restarts, until train is called again.
     assert(~isempty(dataset));
@@ -28,84 +28,52 @@ methods
     dataset= dataset{:,:};
     x= dataset'; t= targetset';
     
-    this.model= fitnet(this.params.hiddenLayerSize,this.params.trainFcn);
-    if nargin < 4, this.configureModel(dataset);
-    else this.configureModel(dataset,repoStarts); end
+    this.model= fitnet(this.params.hiddenLayerSize(2),this.params.trainFcn);
+    this.configureModel(dataset);
     
     %% Autoencoder feature extraction
-    %{
     xtrain= x(:,this.model.divideParam.trainInd);
     autoenc= trainAutoencoder(xtrain,this.params.hiddenLayerSize(1), ...
-      'MaxEpochs',1500, ...
+      'MaxEpochs',2500, ...
       'L2WeightRegularization',0.001, ...
-      'SparsityRegularization',5, ...
-      'SparsityProportion',0.2, ...
+      'SparsityRegularization',10, ...
+      'SparsityProportion',0.05, ...
       'useGPU',true);
     plotWeights(autoenc);
-    y= autoenc.predict(xtrain);
-    autoencMAE= median(abs(y-xtrain))
+    xauto= autoenc.predict(xtrain);
+    autoencMAE= mean(abs(xauto(:)-xtrain(:)))
     % Train regression layer
     features= autoenc.encode(x);
-    [this.model,tr]= train(this.model,features,t, 'useParallel','yes', 'showresources','yes');
+    this.model.trainParam.show= 20;
+    [this.model,tr_regress]= train(this.model,features,t, 'useParallel','yes', 'showresources','yes');
+    regressionLayer= this.model;
+    save('untuned_net', 'autoenc','regressionLayer','tr_regress');
+    % Evaluate without tuning
+    y= this.model(features);
+    rerr= gdivide(gsubtract(y,t), t+eps);
+    trainRerr_untuned= median(abs(rerr(tr_regress.trainInd)))
+    valRerr_untuned= median(abs(rerr(tr_regress.valInd)))
+    testRerr_untuned= median(abs(rerr(tr_regress.testInd)))
+    %% Tune stacked network
     this.model= stack(autoenc,this.model);
-    % Tune stacked network
-    %[this.model,tr]= train(this.model,x,t, 'useParallel','yes', 'showresources','yes');
-    %}
-    %% Train model
-    [this.model,tr]= train(this.model,x,t, 'useParallel','yes', 'showresources','yes','useGPU','no');
+    this.model.trainParam.show= 5;
+    [this.model,tr_stack]= train(this.model,x,t, 'useParallel','no', 'useGPU','yes', 'showresources','yes');
+    tunedModel= this.model;
+    save('tuned_net', 'tunedModel','tr_stack');
     %% Evaluate model
     %
     y= this.model(x);
-    rerr= gdivide(gsubtract(y,t), t+min(t));
-    trainRerr= median(abs(rerr(tr.trainInd)))
-    valRerr= median(abs(rerr(tr.valInd)))
-    testRerr= median(abs(rerr(tr.testInd)))
-    %}
+    rerr= gdivide(gsubtract(y,t), t+eps);
+    trainRerr= median(abs(rerr(tr_stack.trainInd)))
+    valRerr= median(abs(rerr(tr_stack.valInd)))
+    testRerr= median(abs(rerr(tr_stack.testInd)))
+    %
     % Save to file and return evaluation
     this.saveModel();
     this.initialized= true;
     evalResults= struct('trainRerr',trainRerr,'valRerr',valRerr,'testRerr',testRerr);
   end
-%}
-%{
-  function evalResults= train(this, dataset, targetset)
-    % Arrange dataset
-    dataset= dataset{:,:};
-    x= dataset'; t= targetset';
-    nobs= size(dataset,1);
-    trainIdx= randsample(1:nobs, floor(this.params.ratios(1)*nobs));  % numeric
-    testIdx= utils.allbut(nobs, trainIdx);  % logical
-    xtrain= x(:,trainIdx); ttrain= t(trainIdx); xtest= x(:,testIdx); ttest= t(testIdx);
-    % Make classes
-    ctrain= utils.discretizeLevels(ttrain, ttrain);
-    ctest= utils.discretizeLevels(ttest, ttrain);
-    
-    %{
-    % Build autoencoder feature extractor
-    autoenc= trainAutoencoder(xtrain,this.params.hiddenLayerSize(1), ...
-      'MaxEpochs',1000, ...
-      'L2WeightRegularization',0.001, ...
-      'SparsityRegularization',4, ...
-      'SparsityProportion',0.05, ...
-      'DecoderTransferFunction','purelin');
-    plotWeights(autoenc);
-    features= autoenc.encode(xtrain);
-    %}
-    
-    %softnet = trainSoftmaxLayer(features,ctrain,'MaxEpochs',700);
-    %classifier = stack(autoenc,softnet);
-    % Evaluate
-    xrec= autoenc.predict(xtrain);
-    autoencMse= mse(xtrain-xrec)
-    y= classifier(xtest);
-    figure, plotconfusion(ctest,y, 'Before tuning');
-    % Tune
-    classifier= classifier.train(xtrain,ctrain);
-    % Evaluate tuned
-    y= classifier(xtest);
-    plotconfusion(ctest,y, 'After tuning');
-  end
-%}
+
   function y= infer(this, x)
     if this.initialized
       x= x{:,:}';
@@ -115,8 +83,8 @@ methods
     end
   end
   
-  function this= configureModel(this, dataset,repoStarts)
-    this.model.input.processFcns= {'removeconstantrows','mapminmax','processpca'};
+  function this= configureModel(this, dataset)
+    this.model.input.processFcns= {'removeconstantrows','mapminmax'};
     this.model.output.processFcns= {'removeconstantrows','mapminmax'};
     this.model.performFcn= this.params.performFcn;
     this.model.plotParams{3}.bins= 40;
@@ -132,7 +100,6 @@ methods
     %% Divide dataset to train,validation,test parts
     % For the time being, the naive approach of treating each sample independently is followed
     this.params.ratios= this.params.ratios/sum(this.params.ratios);
-    %if nargin < 3
       % If this is a single repo, split among the data
       n= size(dataset,1);
       idx= zeros(n,1); population= 1:n;
@@ -146,28 +113,6 @@ methods
       this.model.divideParam.trainInd= trainIdx;
       this.model.divideParam.valInd= valIdx;
       this.model.divideParam.testInd= testIdx;
-    %{
-    else
-      % If there are multiple repos, then split along the repos (some repos for
-      % training, others for testing)
-      nrepos= length(repoStarts)-1;   % -1 because the last repoStart is the start of the next repo
-      trainRepos= randsample(nrepos, nrepos -floor(this.params.ratios(2)*nrepos) -floor(this.params.ratios(3)*nrepos));
-      valRepos= randsample(nrepos, floor(this.params.ratios(2)*nrepos));
-      testRepos= randsample(nrepos, floor(this.params.ratios(3)*nrepos));
-      
-      trainInd= arrayfun(@(x) (repoStarts(x):repoStarts(x+1)-1), ...
-        trainRepos, 'UniformOutput',false);
-      valInd= arrayfun(@(x) (repoStarts(x):repoStarts(x+1)-1), ...
-        valRepos, 'UniformOutput',false);
-      testInd= arrayfun(@(x) (repoStarts(x):repoStarts(x+1)-1), ...
-        testRepos, 'UniformOutput',false);
-      
-      this.model.divideFcn= 'divideind';
-      this.model.divideParam.trainInd= [trainInd{:}];
-      this.model.divideParam.valInd= [valInd{:}];
-      this.model.divideParam.testInd= [testInd{:}];
-    end
-    %}
   end
 end
 
